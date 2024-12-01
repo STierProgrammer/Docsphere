711f5cb91883e49fac5d1e1a6c71c7109001da5d
STierProgrammer <sabasajaia40@gmail.com>
2024-12-01 15:07:25 +0400

    Few Changes to TipTap Editor!

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import {EditorContent, useEditor} from "@tiptap/react";
-import {StarterKit} from "@tiptap/starter-kit";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 
 export const Editor = () => {
     const editor = useEditor({
@@ -11,7 +13,13 @@ export const Editor = () => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             }
         },
-        extensions: [StarterKit],
+        extensions: [
+            StarterKit,
+            TaskItem.configure({
+                nested: true
+            }),
+            TaskList
+        ],
         content: "<p>Hello, World!</p>"
     });
 
